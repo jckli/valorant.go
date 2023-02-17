0f44b7ba956465696cd512b762b9a45985336d4a
lina <me@jackli.dev>
2023-02-16 21:08:28 -0700

    fix (maybe): reauth?

--- v2/reauth.go
+++ v2/reauth.go
@@ -5,12 +5,18 @@ import (
 )
 
 func Reauthenticate(auth *AuthBody) (*http.Response, error) {
+	reauthHeaders := http.Header{
+		"Content-Type": {"application/json"},
+		"Cookie":       {""},
+		"User-Agent":   {""},
+	}
 	client := &http.Client{}
 	req, err := http.NewRequest(http.MethodGet, "https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1", nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Cookie", auth.Cookies)
-	req.Header.Set("Referer", req.URL.Host)
+	reauthHeaders.Set("Cookie", auth.Cookies)
+	reauthHeaders.Set("Referer", req.URL.Host)
+	req.Header = reauthHeaders
 	return client.Do(req)
 }
