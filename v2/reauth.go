671c605aca6f82fed9c389296656d527675d5276
lina <me@jackli.dev>
2023-02-16 21:13:55 -0700

    fix (maybe, pt2): reauth

--- v2/reauth.go
+++ v2/reauth.go
@@ -2,15 +2,46 @@ package val
 
 import (
 	"net/http"
+	"net"
+	tls "github.com/refraction-networking/utls"
 )
 
-func Reauthenticate(auth *AuthBody) (*http.Response, error) {
-	reauthHeaders := http.Header{
+var (
+	reauthHeaders = http.Header{
 		"Content-Type": {"application/json"},
 		"Cookie":       {""},
 		"User-Agent":   {""},
 	}
-	client := &http.Client{}
+	reauthTlsConfig = tls.Config{
+		CipherSuites: []uint16{tls.TLS_AES_128_GCM_SHA256},
+		MinVersion:   tls.VersionTLS13,
+	}
+)
+
+func reauthDialTLS(network, addr string) (net.Conn, error) {
+	netConn, err := net.Dial(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	config := reauthTlsConfig.Clone()
+	config.ServerName = host
+
+	tlsConn := tls.UClient(netConn, config, tls.HelloGolang)
+	if err = tlsConn.Handshake(); err != nil {
+		return nil, err
+	}
+
+	return tlsConn, nil
+}
+
+func Reauthenticate(auth *AuthBody) (*http.Response, error) {
+	client := &http.Client{Transport: &http.Transport{DialTLS: reauthDialTLS}}
 	req, err := http.NewRequest(http.MethodGet, "https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1", nil)
 	if err != nil {
 		return nil, err
