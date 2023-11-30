8023b75d6c35308e99013b2304485de8cf406a53
lina <me@jackli.dev>
2023-11-30 02:36:10 -0700

    chore: remove comments

--- auth.go
+++ auth.go
@@ -15,7 +15,6 @@ import (
 func generateRandomBytes(n int) ([]byte, error) {
 	b := make([]byte, n)
 	_, err := rand.Read(b)
-	// Note that err == nil only if we read len(b) bytes.
 	if err != nil {
 		return nil, err
 	}
